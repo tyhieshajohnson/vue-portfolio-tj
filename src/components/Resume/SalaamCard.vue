1e4b4a453bcfeecb26398b894f4a6ebd09dcc1e6
tyhieshajohnson <johnsontyhiesha@gmail.com>
2024-01-12 14:24:08 +0200

    JSON Functioning

--- src/components/Resume/SalaamCard.vue
+++ src/components/Resume/SalaamCard.vue
@@ -1,13 +1,25 @@
 <!-- HTML -->
 <template>
-    {{ salaam.id }}
-    {{ salaam.heading }}
-    {{ salaam.text }}
+    <!--  Card 1 -->
+    <div class="card row" style="width: 18rem">
+      <img src="projects.image" class="card-img-top" alt="" />
+      <div class="card-body col">
+        <h5 class="card-title">{{ salaam.school}}</h5>
+        <p class="card-text">{{ salaam.year }}</p>
+        <p class="card-text">{{ salaam.field }}</p>
+      </div>
+  </div>
 </template>
 
 <!-- JavaScript -->
 <script>
-export default {
-    props:["salaam"]
+export default{
+  props:["salaam"]
 }
+
 </script>
+
+<!-- CSS -->
+<style scoped>
+
+</style>
